4d120dc350063dec06667cd132951ac6edb26366
Asteya Tangri <>
2025-06-10 13:14:03 -0700

    3rd TappyPlane Commit

--- Tappy/Scenes/Plane/Plane.gd
+++ Tappy/Scenes/Plane/Plane.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
 
+const JUMP_POWER: float = -350.0
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 var  _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +19,13 @@ func _process(delta: float) -> void:
 	pass
 
 
+
 func _physics_process(delta: float) -> void:
 	velocity.y += _gravity * delta
+	
+	if Input.is_action_just_pressed("jump") == true:
+		velocity.y = JUMP_POWER
+		animation_player.play("jump")
+	
 	
 	move_and_slide()
