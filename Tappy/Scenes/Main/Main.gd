fd21d4723f19c1f2e3a587da86c3e0e380762b9f
Asteya Tangri <>
2025-06-11 16:05:41 -0700

    June 11th 2025 2nd Commit for this date

---
+++ Tappy/Scenes/Main/Main.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _input(event: InputEvent) -> void:
+	print(event)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
