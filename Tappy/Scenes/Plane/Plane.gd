73e49704aa48be80cce90ef5c31d924e5ee6396d
Asteya Tangri <>
2025-06-10 17:37:15 -0700

    TappyPlane Commit

--- Tappy/Scenes/Plane/Plane.gd
+++ Tappy/Scenes/Plane/Plane.gd
@@ -5,6 +5,7 @@ const JUMP_POWER: float = -350.0
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 var  _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
@@ -18,14 +19,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _physics_process(delta: float) -> void:	
+	fly(delta)
+	move_and_slide()	
+	if is_on_floor() == true:
+		die()
 
-
-func _physics_process(delta: float) -> void:
+func fly(delta: float) -> void:
 	velocity.y += _gravity * delta
-	
 	if Input.is_action_just_pressed("jump") == true:
 		velocity.y = JUMP_POWER
 		animation_player.play("jump")
-	
-	
-	move_and_slide()
+
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
