ff3004c0e6c9a6f1dc9f32dbdadf8cae315d3f62
Asteya Tangri <>
2025-07-06 17:58:44 -0700

    snail

--- FoxyAntics/Scenes/Enemies/EnemyBase.gd
+++ FoxyAntics/Scenes/Enemies/EnemyBase.gd
@@ -31,16 +31,14 @@ func _physics_process(delta: float) -> void:
 
 
 
-
-
-
-
-
-
-
-
-
+func die() -> void:
+	set_physics_process(false)
+	queue_free()
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	pass # Replace with function body.
+
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	die()
